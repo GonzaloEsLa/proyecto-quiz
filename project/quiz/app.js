0dced600d4665a6a7257971d00fd8b2fb8522040
Gonzalo Espinosa <gonzalo.espinosa@pixelis.es>
2015-07-07 02:38:27 +0200

    version1
    Primera página y favicon

--- project/quiz/app.js
+++ project/quiz/app.js
@@ -6,16 +6,19 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
+
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,7 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -31,6 +34,10 @@ app.use(function(req, res, next) {
     err.status = 404;
     next(err);
 });
+
+
+
+
 
 // error handlers
 
