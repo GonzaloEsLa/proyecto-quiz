d48d19ab247e1c6a9ff8ba2c702aa64fa2eb2860
Gonzalo Espinosa <gonzalo.espinosa@pixelis.es>
2015-07-12 22:21:24 +0200

    Seccion creditos
    Añadida página author y modificaciones CSS

--- project/quiz/controllers/quiz_controller.js
+++ project/quiz/controllers/quiz_controller.js
@@ -13,3 +13,4 @@ exports.answer= function(req, res) {
     if(req.query.respuesta=== "Roma") res.render('quizes/answer', { respuesta: 'Correcto' });
     else  res.render('quizes/answer', { respuesta: 'Incorrecto' });
 };
+
