07ebe944ed78d2dd4793d01f79a52f7047ef2451
Gonzalo Espinosa <gonzalo.espinosa@pixelis.es>
2015-07-15 20:43:28 +0200

    Autoload y varias preguntas
    Añadido autoload y múltiples preguntas

--- project/quiz/models/models.js
+++ project/quiz/models/models.js
@@ -1,7 +1,6 @@
 var path = require('path');
 
-// Postgres DATABASE_URL = postgres://user:passwd@host:port/database
-// SQLite   DATABASE_URL = sqlite://:@:/
+
 var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
 var DB_name  = (url[6]||null);
 var user     = (url[2]||null);
@@ -11,6 +10,7 @@ var dialect  = (url[1]||null);
 var port     = (url[5]||null);
 var host     = (url[4]||null);
 var storage  = process.env.DATABASE_STORAGE;
+
 
 // Cargar Modelo ORM
 var Sequelize = require('sequelize');
@@ -40,6 +40,8 @@ exports.Quiz = Quiz; // exportar definicion de la tabla Quiz
 
 
 
+
+
 // sequelize.sync crea e inicializa tabla de preguntas en DB
 sequelize.sync().success(function(){
 
@@ -49,13 +51,19 @@ sequelize.sync().success(function(){
     
     
               // ---- si la tabla esta vacia la inicializamos ----
-              if(count==0){
+              if(count===0){
                   
                       Quiz.create({
                                    pregunta: 'Capital de Italia',
                                    respuesta: 'Roma'
+                      });
+                  
+                      Quiz.create({
+                                   pregunta: 'Capital de Portugal',
+                                   respuesta: 'Lisboa'
                       })
-                      .success(function(){console.log('Base de datos inicializada')});
+                      
+                      .then(function(){console.log('Base de datos inicializada')});
                   
               };
                         
--- project/quiz/routes/index.js
+++ project/quiz/routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 
+//Pagina de inicio (home)
 router.get('/', function(req, res) {
     res.render('index', { title: 'Quiz' });
 });
@@ -10,8 +11,14 @@ router.get('/author', function(req, res) {
     res.render('author', {});
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+//Autoload de comandos con :quizId
+router.param('quizId', quizController.load);
+
+
+// Preguntas y respuestas
+router.get('/quizes', quizController.index);  // muestra todas las preguntas
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
   
 
 
