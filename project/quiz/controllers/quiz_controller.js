72eacdb8f26c0213afdb4fde4e612613ab300049
Gonzalo Espinosa <gonzalo.espinosa@pixelis.es>
2015-07-19 23:45:46 +0200

    Corrección errores

--- project/quiz/controllers/quiz_controller.js
+++ project/quiz/controllers/quiz_controller.js
@@ -22,12 +22,20 @@ exports.load = function(req, res, next, quizId) {
 
 
 // GET /quizes
-exports.index= function(req, res) {
-   
-    models.Quiz.findAll().then(function(quizes){
-              res.render('quizes/index.ejs', { quizes: quizes});
-    });
+exports.index = function(req, res) {  
+ 
+      var options = {};
+
+
+      models.Quiz.findAll(options).then(
+      
+        function(quizes) {
+          res.render('quizes/index.ejs', {quizes: quizes, errors: []});
+        }
+          
+      ).catch(function(error){next(error)});
 };
+
 
 
 
